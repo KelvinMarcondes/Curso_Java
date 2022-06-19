88be93a953655763059b5c930a680affa852a029
KelvinMarcondes <kelvindesouza@hotmail.com>
2022-06-19 20:49:05 -0300

    att pastas

---
+++ Curso_part02/src/application/Aula015_AreaTrianguloComPOO.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Aula015_Triangle;
+
+public class Aula015_AreaTrianguloComPOO {
+	
+	/*
+	 * Ler as medidas de dois triangulos e em seguida mostrar o valor das areas  e dizer qual o maior triangulo
+	 * area = sqrt (p(p-a)(p-b)(p-c) onde p= (a+b+c)/2
+	 */
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Aula015_Triangle x,y;
+		x = new Aula015_Triangle();
+		y = new Aula015_Triangle();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println("Enter the measures of triangle Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double p = (x.a + x.b + x.c)/2.0;
+		double areaX = Math.sqrt(p*(p - x.a)*(p - x.b)*(p - x.c));
+		
+		p = (y.a + y.b + y.c)/2.0;
+		double areaY = Math.sqrt(p*(p - y.a)*(p - y.b)*(p - y.c));
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Larger area: X ");
+		}
+		else {
+			System.out.println("Larger area : Y");
+		}
+		sc.close();
+
+	}
+
+}
---
+++ Curso_part02/src/entities/Aula015_Triangle.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class Aula015_Triangle {
+	public double a;
+	public double b;
+	public double c;
+}
